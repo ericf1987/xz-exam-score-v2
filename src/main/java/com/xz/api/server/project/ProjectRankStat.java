230b26ddab19368ae13a70e53e587a47bc81bbff
zrw1988 <464648299@qq.com>
2016-06-01 18:30:07 +0800

    新增数据统计接口

--- src/main/java/com/xz/api/server/project/ProjectRankStat.java
+++ src/main/java/com/xz/api/server/project/ProjectRankStat.java
@@ -1,12 +1,27 @@
 package com.xz.api.server.project;
 
 import com.xz.ajiaedu.common.lang.Result;
+import com.xz.ajiaedu.common.lang.StringUtil;
 import com.xz.api.Param;
 import com.xz.api.annotation.Function;
 import com.xz.api.annotation.Parameter;
 import com.xz.api.annotation.Type;
 import com.xz.api.server.Server;
+import com.xz.bean.Range;
+import com.xz.bean.Target;
+import com.xz.services.RankSegmentService;
+import com.xz.services.SchoolService;
+import com.xz.services.StudentService;
+import com.xz.services.TargetService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 总体成绩-排名统计
@@ -21,13 +36,55 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProjectRankStat implements Server {
 
+    private static Logger LOG = LoggerFactory.getLogger(ProjectRankStat.class);
+
+    @Autowired
+    RankSegmentService rankSegmentService;
+
+    @Autowired
+    SchoolService schoolService;
+
+    @Autowired
+    StudentService studentService;
+
+    @Autowired
+    TargetService targetService;
+
     @Override
     public Result execute(Param param) throws Exception {
         String projectId = param.getString("projectId");
         String subjectId = param.getString("subjectId");
         String[] schoolIds = param.getStringValues("schoolIds");
 
-        //// TODO: 2016/5/30 排名统计
-        return Result.success();
+        List<Map<String, Object>> schoolRankSegments = getSchoolRankSegments(projectId, subjectId, schoolIds);
+        return Result.success().set("schools", schoolRankSegments);
+    }
+
+    // 学校排名分段统计
+    private List<Map<String, Object>> getSchoolRankSegments(String projectId, String subjectId, String[] schoolIds) {
+        List<Map<String, Object>> schoolRankSegments = new ArrayList<>();
+
+        for (String schoolId : schoolIds) {
+            String schoolName = schoolService.queryExamSchoolName(projectId, schoolId);
+            if (StringUtil.isBlank(schoolName)) {
+                LOG.warn("找不到学校:'{}'的考试记录", schoolId);
+                return null;
+            }
+            Map<String, Object> map = new HashMap<>();
+            map.put("schoolName", schoolName);
+
+            // 考生人数
+            Range range = Range.school(schoolId);
+            Target target = targetService.getTarget(projectId, subjectId);
+            map.put("studentCount",  studentService.getStudentCount(projectId, range, target));
+
+            // 排行分段
+            List<Map<String, Object>> rankStat = rankSegmentService.queryFullRankSegment(projectId, target, range);
+            map.put("rankStat", rankStat);
+
+            schoolRankSegments.add(map);
+        }
+
+        return schoolRankSegments;
     }
 }
--- src/main/java/com/xz/services/RankSegmentService.java
+++ src/main/java/com/xz/services/RankSegmentService.java
@@ -8,8 +8,7 @@ import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * 查询排名分段比率
@@ -22,8 +21,45 @@ public class RankSegmentService {
     @Autowired
     MongoDatabase scoreDatabase;
 
-    @Autowired
-    ProjectConfigService projectConfigService;
+    /**
+     * 查询完整的排行分段
+     *
+     * @param projectId 考试项目id
+     * @param target    目标
+     * @param range     范围
+     *
+     * @return  完整的排行分段
+     */
+    public List<Map<String, Object>> queryFullRankSegment(String projectId, Target target, Range range) {
+        List<Document> rankSegments = getRankSegments(projectId, range, target);
+        rankSegments.sort((o1, o2) -> o1.getDouble("rankPercent").compareTo(o2.getDouble("rankPercent")));
+
+        double totalRate = 0;
+        int minPercent, maxPercent= 0;
+        List<Map<String, Object>> rankStats = new ArrayList<>();
+
+        for (Document rankSegment : rankSegments) {
+            Map<String, Object> map = new HashMap<>();
+
+            double rankPercent = rankSegment.getDouble("rankPercent");
+            double rate = rankSegment.getDouble("rate");
+            int count = rankSegment.getInteger("count");
+            minPercent = maxPercent;
+            maxPercent = (int) (rankPercent * 100);
+            totalRate += rate;
+
+            map.put("minPercent", minPercent);
+            map.put("maxPercent", maxPercent);
+            map.put("count", count);
+            map.put("rate", rate);
+            map.put("rankPercent", rankPercent);
+            map.put("totalRate", totalRate);
+
+            rankStats.add(map);
+        }
+
+        return rankStats;
+    }
 
     @SuppressWarnings("unchecked")
     public List<Document> getRankSegments(String projectId, Range range, Target target) {
