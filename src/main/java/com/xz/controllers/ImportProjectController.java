c8f2bd19e51dce5a5ff73394a613c2660468d88f
yiding he <yiding.he@gmail.com>
2016-06-03 15:14:06 +0800

    导入项目（未完成）

--- src/main/java/com/xz/controllers/ImportProjectController.java
+++ src/main/java/com/xz/controllers/ImportProjectController.java
@@ -2,6 +2,7 @@ package com.xz.controllers;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.xz.ajiaedu.common.beans.exam.ExamProject;
 import com.xz.ajiaedu.common.beans.user.School;
 import com.xz.ajiaedu.common.lang.Context;
 import com.xz.ajiaedu.common.lang.Result;
@@ -9,8 +10,10 @@ import com.xz.api.Param;
 import com.xz.bean.Target;
 import com.xz.intclient.InterfaceClient;
 import com.xz.services.FullScoreService;
+import com.xz.services.ProjectService;
 import com.xz.services.SchoolService;
 import com.xz.services.SubjectService;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.xz.ajiaedu.common.mongo.MongoUtils.doc;
 
 /**
  * 湘潭联考：430300-672a0ed23d9148e5a2a31c8bf1e08e62
@@ -32,6 +37,9 @@ public class ImportProjectController {
 
     @Autowired
     InterfaceClient interfaceClient;
+
+    @Autowired
+    ProjectService projectService;
 
     @Autowired
     SchoolService schoolService;
@@ -49,23 +57,27 @@ public class ImportProjectController {
 
         Context context = new Context();
 
+        // 下面的导入顺序不能变更，否则可能造成数据错误
         importProjectInfo(projectId);
         importSubjects(projectId);
         importQuests(projectId);
-        importSchools(projectId);
-        importStudents(projectId);
+        importSchools(projectId, context);
+        importStudents(projectId, context);
 
         return Result.success();
     }
 
-    private void importStudents(String projectId) {
+    private void importStudents(String projectId, Context context) {
+        List<Document> schools = context.get("schools");
 
     }
 
-    private void importSchools(String projectId) {
+    private void importSchools(String projectId, Context context) {
         Param param = new Param().setParameter("projectId", projectId);
         Result result = interfaceClient.request("QueryExamSchoolByProject", param);
         JSONArray jsonArray = result.get("schools");
+
+        List<Document> schoolList = new ArrayList<>();  // 存入 project_list
 
         jsonArray.forEach(o -> {
             JSONObject schoolObj = (JSONObject) o;
@@ -77,7 +89,11 @@ public class ImportProjectController {
             school.setProvince(schoolObj.getString("province"));
 
             schoolService.saveProjectSchool(projectId, school);
+            schoolList.add(doc("school", school.getId()).append("name", school.getName()));
         });
+
+        context.put("schools", schoolList);  // 导入学生时要用到
+        projectService.saveProjectSchools(projectId, schoolList);
     }
 
     private void importQuests(String projectId) {
@@ -110,6 +126,15 @@ public class ImportProjectController {
     }
 
     private void importProjectInfo(String projectId) {
+        Param param = new Param().setParameter("projectId", projectId);
+        Result result = interfaceClient.request("QueryProjectById", param);
 
+        JSONObject projectObj = result.get("result");
+        ExamProject project = new ExamProject();
+        project.setId(projectId);
+        project.setName(projectObj.getString("name"));
+        project.setGrade(projectObj.getInteger("grade"));
+
+        projectService.saveProject(project);
     }
 }
--- src/main/java/com/xz/services/ProjectService.java
+++ src/main/java/com/xz/services/ProjectService.java
@@ -1,7 +1,9 @@
 package com.xz.services;
 
 import com.hyd.simplecache.SimpleCache;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.xz.ajiaedu.common.beans.exam.ExamProject;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.xz.ajiaedu.common.mongo.MongoUtils.doc;
+import static com.xz.ajiaedu.common.mongo.MongoUtils.*;
 
 /**
  * 考试项目业务类
@@ -31,7 +33,7 @@ public class ProjectService {
      * @param schoolId  学校id
      * @param examMonth 考试月份 格式 yyyy-MM
      *
-     * @return  考试项目列表
+     * @return 考试项目列表
      */
     List<Document> querySchoolProjects(String schoolId, String examMonth) {
         String cacheKey = "school_projects:" + schoolId + ":" + examMonth;
@@ -43,5 +45,29 @@ public class ProjectService {
 //        })
 
         return new ArrayList<>();
+    }
+
+    /**
+     * 保存项目信息（不包含学校列表）
+     *
+     * @param project 项目信息
+     */
+    public void saveProject(ExamProject project) {
+        MongoCollection<Document> c = scoreDatabase.getCollection("project_list");
+        Document query = doc("project", project.getId());
+        Document update = doc("name", project.getName()).append("grade", project.getGrade());
+        c.updateOne(query, $set(update), UPSERT);
+    }
+
+    /**
+     * 保存学校列表
+     *
+     * @param projectId  项目ID
+     * @param schoolList 学校列表（name 和 school 属性）
+     */
+    public void saveProjectSchools(String projectId, List<Document> schoolList) {
+        MongoCollection<Document> c = scoreDatabase.getCollection("project_list");
+        Document query = doc("project", projectId);
+        c.updateOne(query, $set("schools", schoolList));
     }
 }
