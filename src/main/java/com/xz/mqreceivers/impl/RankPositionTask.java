8ce29f85479320bb4f67b0b57a9263aa51b9c1e7
yiding he <yiding.he@gmail.com>
2016-06-06 19:08:12 +0800

    ................

--- src/main/java/com/xz/mqreceivers/impl/RankPositionTask.java
+++ src/main/java/com/xz/mqreceivers/impl/RankPositionTask.java
@@ -2,7 +2,6 @@ package com.xz.mqreceivers.impl;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.xz.ajiaedu.common.mongo.MongoUtils;
 import com.xz.bean.Range;
 import com.xz.bean.Target;
 import com.xz.mqreceivers.AggrTask;
@@ -15,18 +14,19 @@ import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.print.Doc;
-import javax.print.attribute.IntegerSyntax;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import static com.xz.ajiaedu.common.mongo.MongoUtils.$set;
+import static com.xz.ajiaedu.common.mongo.MongoUtils.UPSERT;
 
 /**
  * @author by fengye on 2016/5/27.
  */
 @ReceiverInfo(taskType = "rank_position")
 @Component
-public class RankPositionTask extends Receiver{
+public class RankPositionTask extends Receiver {
 
     @Autowired
     RangeService rangeService;
@@ -38,70 +38,73 @@ public class RankPositionTask extends Receiver{
     MongoDatabase scoreDataBase;
 
     //统计指标
-    static final String [] POSITIONS = new String[]{"1/4","2/4","3/4"};
+    static final String[] POSITIONS = new String[]{"1/4", "2/4", "3/4"};
 
+    @SuppressWarnings("unchecked")
     @Override
     protected void runTask(AggrTask aggrTask) {
-        //获取
         String projectId = aggrTask.getProjectId();
-        Range range = aggrTask.getRange();
         Target target = aggrTask.getTarget();
+        Range range = aggrTask.getRange();
 
         //从总分表中取源数据
-        MongoCollection<Document> scoreCol = scoreDataBase.getCollection("score_map");
+        MongoCollection<Document> scoreMapCollection = scoreDataBase.getCollection("score_map");
         //目的数据表
-        MongoCollection<Document> rankPositionCol = scoreDataBase.getCollection("rank_position");
+        MongoCollection<Document> rankPositionCollection = scoreDataBase.getCollection("rank_position");
 
         Document query = new Document("project", projectId).
                 append("range", Mongo.range2Doc(range)).
                 append("target", Mongo.target2Doc(target));
 
-        List<Document> positions = new ArrayList<Document>();
-        Document oneScoreMapDoc = scoreCol.find(query).first();
+        List<Document> positions = new ArrayList<>();
+        Document scoreMapDoc = scoreMapCollection.find(query).first();
+
         //获取总人数
-        int count = oneScoreMapDoc.getInteger("count");
+        int count = scoreMapDoc.getInteger("count");
+        if (count == 0) {
+            throw new IllegalStateException("没有排名信息：" + query.toJson());
+        }
 
         //获取总分表中的scoreMap节点
-        List<Document> scoreMap = (List<Document>)oneScoreMapDoc.get("scoreMap");
-        for(String s : POSITIONS){
-            double score = getScoreAtIndex(scoreMap, count, s2d(s));
-            Document position = new Document("position", s2d(s)).append("score", score);
+        List<Document> scoreMap = (List<Document>) scoreMapDoc.get("scoreMap");
+
+        for (String s : POSITIONS) {
+            double score = getScoreAtIndex(scoreMap, count, parsePosition(s));
+            Document position = new Document("position", parsePosition(s)).append("score", score);
             positions.add(position);
         }
 
-        rankPositionCol.deleteMany(query);
-        rankPositionCol.updateMany(
-                query,
-                MongoUtils.$set("positions", positions),
-                MongoUtils.UPSERT
-        );
-
+        rankPositionCollection.deleteMany(query);
+        rankPositionCollection.updateMany(query, $set("positions", positions), UPSERT);
     }
 
-    private Double s2d(String s){
-        String[] arr = s.split("/");
-        Double d = Double.valueOf(arr[0])/Double.valueOf(arr[1]);
-        return d;
+    private Double parsePosition(String positionName) {
+        String[] arr = positionName.split("/");
+        return Double.valueOf(arr[0]) / Double.valueOf(arr[1]);
     }
 
-    private double getScoreAtIndex(List<Document> scoreMap, int count, double d) {
-        double score = 0;
-        Collections.sort(scoreMap, (Document d1, Document d2) -> {
-            return d2.getDouble("score").compareTo(d1.getDouble("score"));
-        });
-        int index = Double.valueOf(count * d).intValue();
-        //如果一个班小于4个人，做出判断
-        if(index <= 1){
-            return scoreMap.get(index).getDouble("score");
+    private double getScoreAtIndex(List<Document> scoreMap, int count, double rate) {
+
+        // 按照分数倒排 map 元素
+        Collections.sort(scoreMap, (d1, d2) -> d2.getDouble("score").compareTo(d1.getDouble("score")));
+
+        // 极端情况下取一个人
+        int index = Double.valueOf(count * rate).intValue();
+        if (index <= 1) {
+            return scoreMap.get(0).getDouble("score");
         }
-        int oneCount = 0;
-        for(Document item : scoreMap){
-            oneCount += item.getInteger("count");
-            if(oneCount >= index){
+
+        double score = 0;
+        int counter = 0;
+
+        for (Document item : scoreMap) {
+            counter += item.getInteger("count");
+            if (counter >= index) {
                 return item.getDouble("score");
             }
             score = item.getDouble("score");
         }
+
         return score;
     }
 }
