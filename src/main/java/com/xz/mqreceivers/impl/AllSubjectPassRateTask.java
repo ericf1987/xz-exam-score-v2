786a51bc8952a340ce9dfd94036e77b01f6fbb82
fengye <talkweb23!@#>
2016-07-04 15:09:12 +0800

    全科通过率任务中增加对全科优秀率和良好率的统计功能

--- src/main/java/com/xz/mqreceivers/impl/AllSubjectPassRateTask.java
+++ src/main/java/com/xz/mqreceivers/impl/AllSubjectPassRateTask.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 import static com.xz.ajiaedu.common.mongo.MongoUtils.doc;
-import static com.xz.ajiaedu.common.report.Keys.ScoreLevel.Fail;
+import static com.xz.ajiaedu.common.report.Keys.ScoreLevel.*;
 
 @Component
 @ReceiverInfo(taskType = "all_subject_pass_rate")
@@ -45,17 +45,24 @@ public class AllSubjectPassRateTask extends Receiver {
         List<String> subjectIds = subjectService.querySubjects(projectId);
         List<String> studentList = studentService.getStudentIds(projectId, range, Target.project(projectId));
 
-        // 计算全科及格和不及格人数
+        // 计算全科及格和不及格人数和优秀人数以及良好人数
         CounterMap<String> passCounter = new CounterMap<>();
 
         for (String studentId : studentList) {
-            boolean allPass = true, allFail = true;
+            boolean allPass = true, allFail = true, allExcellent = true, allGood = true;
 
             for (String subjectId : subjectIds) {
                 String scoreLevel = scoreLevelService.getScoreLevel(projectId, studentId, Target.subject(subjectId));
-                if (scoreLevel.equals(Fail.name())) {
+                if (!scoreLevel.equals(Pass.name())) {
                     allPass = false;
-                } else {
+                }
+                if (!scoreLevel.equals(Excellent.name())) {
+                    allExcellent = false;
+                }
+                if (!scoreLevel.equals(Good.name())) {
+                    allGood = false;
+                }
+                if (!scoreLevel.equals(Fail.name())) {
                     allFail = false;
                 }
             }
@@ -66,15 +73,27 @@ public class AllSubjectPassRateTask extends Receiver {
             if (allFail) {
                 passCounter.incre("allFail");
             }
+            if (allExcellent) {
+                passCounter.incre("allExcellent");
+            }
+            if (allGood) {
+                passCounter.incre("allGood");
+            }
         }
 
-        // 计算全科及格和不及格比例
+        // 计算相应比例
         double allPassRate = (double) (passCounter.getCount("allPass")) / studentList.size();
         double allFailRate = (double) (passCounter.getCount("allFail")) / studentList.size();
+        double allExcellentRate = (double) (passCounter.getCount("allExcellent")) / studentList.size();
+        double allGoodRate = (double) (passCounter.getCount("allGood")) / studentList.size();
 
         Document query = doc("project", projectId).append("range", Mongo.range2Doc(range));
         MongoCollection<Document> c = scoreDatabase.getCollection("all_subject_pass_rate");
         c.deleteMany(query);
-        c.insertOne(doc(query).append("allPassRate", allPassRate).append("allFailRate", allFailRate));
+        c.insertOne(doc(query)
+                .append("allPassRate", allPassRate)
+                .append("allFailRate", allFailRate)
+                .append("allExcellentRate", allExcellentRate)
+                .append("allGoodRate", allGoodRate));
     }
 }
