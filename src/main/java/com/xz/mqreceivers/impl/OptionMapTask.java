16da2d527ca355d1149303729ade71f6e7178503
fengye <talkweb23!@#>
2016-07-06 18:14:11 +0800

    客观题选项选择率功能调整

--- src/main/java/com/xz/mqreceivers/impl/OptionMapTask.java
+++ src/main/java/com/xz/mqreceivers/impl/OptionMapTask.java
@@ -55,19 +55,22 @@ public class OptionMapTask extends Receiver {
         int studentCount = studentService.getStudentCount(projectId, range);
 
         aggregate.forEach((Consumer<Document>) document -> {
-            String answer = ((Document) document.get("_id")).getString("answer");
-            int count;
-            double rate;
+            List<Document> newOptionMapList = convertOneDoc(document);
+            for(Document newOptionMap : newOptionMapList){
+                String answer = ((Document) newOptionMap.get("_id")).getString("answer");
+                int count;
+                double rate;
 
-            if (studentCount == 0) {
-                count = 0;
-                rate = 0;
-            } else {
-                count = document.getInteger("count");
-                rate = (double) count / studentCount;
+                if (studentCount == 0) {
+                    count = 0;
+                    rate = 0;
+                } else {
+                    count = newOptionMap.getInteger("count");
+                    rate = (double) count / studentCount;
+                }
+
+                addUpToList(optionMapList, answer, count, rate, studentCount);
             }
-
-            addUpToList(optionMapList, answer, count, rate);
         });
 
         Document query = query(projectId, range).append("quest", questId);
@@ -75,15 +78,30 @@ public class OptionMapTask extends Receiver {
         optionMapCollection.updateOne(query, update, UPSERT);
     }
 
-    private void addUpToList(List<Document> optionMapList, String answer, int studentCount, double rate) {
+    private void addUpToList(List<Document> optionMapList, String answer, int oneCount, double rate, int studentCount) {
         for (Document document : optionMapList) {
             if (document.getString("answer").equals(answer)) {
-                document.put("count", studentCount);
-                document.put("rate", rate);
+                //如果答案选项已经存在，则累加count，同时修改rate
+                int count = document.get("count").toString() == null ? 0 : Integer.parseInt(document.get("count").toString());
+                document.put("count", count + oneCount);
+                document.put("rate", (double) (count + oneCount) / studentCount);
                 return;
             }
         }
 
+        //如果没有找到选项，则新增选项
         optionMapList.add(doc("answer", answer).append("count", studentCount).append("rate", rate));
+    }
+
+    //将answer为多个选项的doc转化成多个单选项的组成的list
+    private List<Document> convertOneDoc(Document optionMap){
+        List<Document> newOptionMapList = new ArrayList<>();
+        String answer = ((Document) optionMap.get("_id")).getString("answer");
+        int count = optionMap.getInteger("count");
+        char[] arr = answer.toCharArray();
+        for (char a : arr){
+            newOptionMapList.add(new Document().append("_id", doc("answer", String.valueOf(a))).append("count", count));
+        }
+        return newOptionMapList;
     }
 }
