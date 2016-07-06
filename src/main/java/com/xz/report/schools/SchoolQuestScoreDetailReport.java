e35c5a0c3241dc987bf435b9d4b5910c064c01d8
fengye <talkweb23!@#>
2016-07-06 15:13:37 +0800

    各科试卷题目明细得分报表格式调整 清理警告提示

--- src/main/java/com/xz/report/schools/SchoolQuestScoreDetailReport.java
+++ src/main/java/com/xz/report/schools/SchoolQuestScoreDetailReport.java
@@ -5,6 +5,7 @@ import com.xz.bean.Target;
 import com.xz.report.ReportGenerator;
 import com.xz.report.SheetTask;
 import com.xz.report.classes.ReportGeneratorInfo;
+import com.xz.services.SubjectService;
 import com.xz.services.TargetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -24,11 +25,11 @@ public class SchoolQuestScoreDetailReport extends ReportGenerator{
     @Override
     protected List<SheetTask> getSheetTasks(String projectId, Range range) {
         List<SheetTask> sheetTasks = new ArrayList<>();
-        List<Target> points = targetService.queryTargets(projectId, Target.POINT);
-        for(Target point : points){
-            String subjectName = point.getName();
+        List<Target> subjects = targetService.queryTargets(projectId, Target.SUBJECT);
+        for(Target subject : subjects){
+            String subjectName = SubjectService.getSubjectName(subject.getId().toString());
             SheetTask sheetTask = new SheetTask(subjectName, SchoolQuestScoreDetailSheets.class);
-            sheetTask.put("target", point);
+            sheetTask.put("target", subject);
             sheetTasks.add(sheetTask);
         }
         return sheetTasks;
