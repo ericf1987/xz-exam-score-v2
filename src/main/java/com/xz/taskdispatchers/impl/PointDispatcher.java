4a381ef651bdaddbc6ae7d44b5221f6e29e1ad30
yiding he <yiding.he@gmail.com>
2016-07-11 17:32:03 +0800

    尝试修复平均分问题

--- src/main/java/com/xz/taskdispatchers/impl/PointDispatcher.java
+++ src/main/java/com/xz/taskdispatchers/impl/PointDispatcher.java
@@ -1,17 +1,18 @@
 package com.xz.taskdispatchers.impl;
 
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.xz.bean.ProjectConfig;
 import com.xz.bean.Target;
 import com.xz.services.StudentService;
 import com.xz.taskdispatchers.TaskDispatcher;
 import com.xz.taskdispatchers.TaskDispatcherInfo;
+import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import static com.xz.ajiaedu.common.mongo.MongoUtils.$in;
 import static com.xz.ajiaedu.common.mongo.MongoUtils.doc;
 
 @TaskDispatcherInfo(taskType = "point", isAdvanced = true)
@@ -31,9 +32,10 @@ public class PointDispatcher extends TaskDispatcher {
 
         // 删除旧数据
         LOG.info("删除项目 {} 的 point 相关旧数据...", projectId);
-        scoreDatabase.getCollection("total_score").deleteMany(
-                doc("project", projectId).append("target.name",
-                        $in(Target.POINT, Target.POINT_LEVEL, Target.SUBJECT_LEVEL)));
+        MongoCollection<Document> c = scoreDatabase.getCollection("total_score");
+        c.deleteMany(doc("project", projectId).append("target.name", Target.POINT));
+        c.deleteMany(doc("project", projectId).append("target.name", Target.POINT_LEVEL));
+        c.deleteMany(doc("project", projectId).append("target.name", Target.SUBJECT_LEVEL));
 
         LOG.info("项目 {} 的 point 相关旧数据删除完毕，开始统计...", projectId);
         dispatchTaskForEveryStudent(projectId, aggregationId, studentService);
