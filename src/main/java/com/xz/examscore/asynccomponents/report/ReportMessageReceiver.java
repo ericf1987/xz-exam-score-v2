b00a80f96012500426142788de9a2fed179ef9e6
fengye <talkweb23!@#>
2016-08-31 10:39:00 +0800

    执行生成报表操作没有更新统计状态BUG修复

--- src/main/java/com/xz/examscore/asynccomponents/report/ReportMessageReceiver.java
+++ src/main/java/com/xz/examscore/asynccomponents/report/ReportMessageReceiver.java
@@ -1,11 +1,15 @@
 package com.xz.examscore.asynccomponents.report;
 
 import com.xz.examscore.asynccomponents.MessageReceiver;
+import com.xz.examscore.services.ProjectStatusService;
 import com.xz.examscore.services.ReportService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import static com.xz.examscore.bean.ProjectStatus.ReportGenerated;
+import static com.xz.examscore.bean.ProjectStatus.ReportGenerating;
 
 /**
  * (description)
@@ -21,8 +25,14 @@ public class ReportMessageReceiver extends MessageReceiver<ReportTaskMessage> {
     @Autowired
     ReportService reportService;
 
+    @Autowired
+    ProjectStatusService projectStatusService;
+
     @Override
     protected void executeTask(ReportTaskMessage message) {
+        projectStatusService.setProjectStatus(message.getProjectId(), ReportGenerating);
         reportService.generateReports(message.getProjectId(), true);
+        projectStatusService.setProjectStatus(message.getProjectId(), ReportGenerated);
+
     }
 }
