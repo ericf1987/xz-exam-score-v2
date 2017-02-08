07e916609fe114d1c7df8fa71d230029750c710e
fengye <talkweb23!@#>
2017-02-08 15:17:48 +0800

    记录redis中每个任务队列的执行时间

--- src/test/java/com/xz/examscore/asynccomponents/QueueServiceTest.java
+++ src/test/java/com/xz/examscore/asynccomponents/QueueServiceTest.java
@@ -2,14 +2,9 @@ package com.xz.examscore.asynccomponents;
 
 import com.xz.ajiaedu.common.redis.Redis;
 import com.xz.examscore.XzExamScoreV2ApplicationTests;
-import com.xz.examscore.services.QuestService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-
-import java.util.Set;
-
-import static org.junit.Assert.*;
 
 /**
  * @author by fengye on 2016/10/9.
@@ -43,10 +38,6 @@ public class QueueServiceTest extends XzExamScoreV2ApplicationTests {
 
     @Test
     public void test() {
-/*        Set<String> keys = redis.keys("aggregation_task_count_*");
-        for(String key : keys){
-            redis.delete(key);
-        }*/
         String key = "aggregation_task_count_tttttest:80934c78-7224-4a28-b817-5787325c1de0";
         redis.delete(key);
     }
