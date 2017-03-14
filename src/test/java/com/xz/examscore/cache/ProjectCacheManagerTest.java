28ae3489f6ebbcbc5fa63563e099071c1ed981b2
fengye <talkweb23!@#>
2017-03-14 10:23:50 +0800

    学生试卷留痕截图保存至本地文件系统

--- src/test/java/com/xz/examscore/cache/ProjectCacheManagerTest.java
+++ src/test/java/com/xz/examscore/cache/ProjectCacheManagerTest.java
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-package com.xz.examscore.cache;
-
-import com.hyd.simplecache.SimpleCache;
-import com.xz.examscore.XzExamScoreV2ApplicationTests;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.junit.Assert.*;
-
-/**
- * @author by fengye on 2017/3/8.
- */
-public class ProjectCacheManagerTest extends XzExamScoreV2ApplicationTests{
-
-    @Autowired
-    ProjectCacheManager projectCacheManager;
-
-    @Test
-    public void testGetProjectCache() throws Exception {
-        String projectId = "22222";
-        SimpleCache simpleCache = projectCacheManager.getProjectCache(projectId);
-        simpleCache.put("name", "eric");
-        System.out.println(simpleCache.get("name").toString());
-
-        projectCacheManager.deleteProjectCache(projectId);
-        System.out.println(simpleCache.get("name").toString());
-    }
-
-    @Test
-    public void testDeleteProjectCache() throws Exception {
-
-    }
-=======
 package com.xz.examscore.cache;
 
 import com.hyd.simplecache.SimpleCache;
@@ -63,5 +29,4 @@ public class ProjectCacheManagerTest extends XzExamScoreV2ApplicationTests {
         cache.put("name", "123456");
         System.out.println((String)cache.get("name"));
     }
->>>>>>> origin/master
 }
